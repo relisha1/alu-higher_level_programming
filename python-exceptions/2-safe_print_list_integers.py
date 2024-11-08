49046157d08ec50c234b10172b939ca20167391d
relisha1 <e.rurangwa@alustudent.com>
2024-11-08 23:20:53 +0200

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        quotient = a / b
-    except (TypeError, ZeroDivisionError):
-        quotient = None
-    finally:
-        print("Inside result: {}".format(quotient))
-        return quotient
+def safe_print_list_integers(my_list=[], x=0):
+    num_returned = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num_returned += 1
+        except(TypeError, ValueError):
+            pass
+    print()
+    return num_returned
