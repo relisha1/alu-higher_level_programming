afd3fc95f3878b40eac1d6ecead9c1975be97b1d
relisha1 <e.rurangwa@alustudent.com>
2024-11-16 15:33:34 +0000

    add

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Return the Python object representation of a JSON string."""
+""" writes an Object to a text file, using a JSON representation:."""
 import json
 
 
-def from_json_string(my_str):
-    """Return the Python object representation of a JSON string."""
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
