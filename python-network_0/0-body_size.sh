e66a209eb3d653e0a3cd1cc7462a6a94074b49a2
relisha1 <e.rurangwa@alustudent.com>
2024-11-30 11:06:33 +0000

    add

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# 0-body_size.sh - This script takes a URL, sends a request, and displays the size of the body of the response
-
-# Check if a URL argument was provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send the request and get the body size
-curl -s -o /dev/null -w "%{size_download}" "$1"
-
+# Sends a GET request to a URL, and displays the body of the response
+curl -sL "$1"
