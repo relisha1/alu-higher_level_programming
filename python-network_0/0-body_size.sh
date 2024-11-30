8294ce04f1f4e566cf554052e554cb1c7d7eadd5
relisha1 <e.rurangwa@alustudent.com>
2024-11-30 15:06:09 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL, and displays the body of the response
-curl -sL "$1"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
